fe3e20db2835fa6b1f6a48bf334cd4d4828436e7
Jannes Brinckmann <jannes.brinckmann@proton.me>
2026-05-22 23:39:28 +0200

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,26 +1,25 @@
-const CACHE='offlinearcade-v1';
+const CACHE='offlinearcade-v2'; // Bei echten Änderungen hochzählen!
 const ASSETS=[
-  'index.html',
-  'style.css',
-  'main.js',
-  'manifest.json',
+  'index.html','style.css','main.js','manifest.json',
   'games/escape-road/index.html',
   'games/escape-road/game.js',
   'games/escape-road/style.css'
-  // ggf. später weitere Assets!
+  // ggf. weitere Assets hier ergänzen!
 ];
+
+// Message-Handler für SkipWaiting bei Update
+self.addEventListener('message',event=>{
+  if(event.data && event.data.type==='SKIP_WAITING'){self.skipWaiting();}
+});
+
 self.addEventListener('install',e=>{
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(c=>c.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
 self.addEventListener('activate',e=>{
   clients.claim();
   e.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
 });
 self.addEventListener('fetch',e=>{
