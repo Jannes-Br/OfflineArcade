7ff225df4ae119402fd5d217e4c22bdf16d1bb3e
Jannes Brinckmann <jannes.brinckmann@proton.me>
2026-05-22 23:55:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,14 @@
-const CACHE='offlinearcade-v2'; // Bei echten Änderungen hochzählen!
+const CACHE='offlinearcade-v4'; // Hochzählen bei echtem Update!
 const ASSETS=[
   'index.html','style.css','main.js','manifest.json',
   'games/escape-road/index.html',
   'games/escape-road/game.js',
   'games/escape-road/style.css'
-  // ggf. weitere Assets hier ergänzen!
 ];
 
-// Message-Handler für SkipWaiting bei Update
 self.addEventListener('message',event=>{
   if(event.data && event.data.type==='SKIP_WAITING'){self.skipWaiting();}
 });
-
 self.addEventListener('install',e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
