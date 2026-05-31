7305d5a849a7e46851cf2829fff8acbc23b760b7
Jannes Brinckmann <jannes.brinckmann@proton.me>
2026-05-31 21:52:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v74';
+const CACHE_VERSION = 'v75';
 const GAMES = ['escape-road', 'drive-mad', 'block-smasher', 'tic-tac-toe', '2048', 'pong'];
 
 document.addEventListener('DOMContentLoaded', () => {
