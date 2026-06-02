0fd8d7782a4b5642ba1b5104daeb2a3973f62326
Jannes Brinckmann <jannes.brinckmann@proton.me>
2026-06-02 17:12:24 +0200

    Update cache version to offlinearcade-v81

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'offlinearcade-v80';
+const CACHE = 'offlinearcade-v81';
 
 // Essenzielle App-Shell Dateien (SW schlägt fehl, wenn diese nicht geladen werden können)
 const ESSENTIAL_ASSETS = [
